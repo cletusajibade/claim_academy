044ad60ac3636671d7a74719f2831e813ce6eb27
Cletus Ajibade <cletusajibade@gmail.com>
2026-09-14 11:24:56 -0500

    made anoterh update

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Cletus";
+        System.out.println("Hello, World!");
+        System.out.println("My name is " + name);
+        System.out.println("I am learning Java.");
+        // TODO: Print one learning goal.
+    }
+}
